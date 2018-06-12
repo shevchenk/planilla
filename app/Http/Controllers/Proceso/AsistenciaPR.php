d78876f64b88a1cf60e986e9c55cbf8687a5f14f
Renzo CB <rcapchab@gmail.com>
2018-06-11 21:14:49 -0500

    Reporte

--- app/Http/Controllers/Proceso/AsistenciaPR.php
+++ app/Http/Controllers/Proceso/AsistenciaPR.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Proceso\Asistencia;
 use App\Models\Proceso\EventoAsistencia;
+use Illuminate\Support\Facades\Validator;
 
 class AsistenciaPR extends Controller
 {
@@ -12,6 +13,66 @@ class AsistenciaPR extends Controller
     {
         $this->middleware('auth');  //Esto debe activarse cuando estemos con sessión
     } 
+    
+       public function New(Request $r )
+    {
+        if ( $r->ajax() ) {
+
+            $mensaje= array(
+                'required'    => ':attribute es requerido',
+                'unique'      => ':attribute solo debe ser único',
+            );
+
+            $rules = array(
+                'fecha_salida' =>
+                       ['required',
+                        ],
+            );
+
+
+            $validator=Validator::make($r->all(), $rules,$mensaje);
+
+            if ( !$validator->fails() ) {
+                Asistencia::runNew($r);
+                $return['rst'] = 1;
+                $return['msj'] = 'Registro creado';
+            }
+            else{
+                $return['rst'] = 2;
+                $return['msj'] = $validator->errors()->all()[0];
+            }
+            return response()->json($return);
+        }
+    }
+
+    public function Edit(Request $r )
+    {
+        if ( $r->ajax() ) {
+            $mensaje= array(
+                'required'    => ':attribute es requerido',
+                'unique'        => ':attribute solo debe ser único',
+            );
+
+            $rules = array(
+                'fecha_salida' =>
+                       ['required',
+                        ],
+            );
+
+            $validator=Validator::make($r->all(), $rules,$mensaje);
+
+            if ( !$validator->fails() ) {
+                Asistencia::runEdit($r);
+                $return['rst'] = 1;
+                $return['msj'] = 'Registro actualizado';
+            }
+            else{
+                $return['rst'] = 2;
+                $return['msj'] = $validator->errors()->all()[0];
+            }
+            return response()->json($return);
+        }
+    }
 
     public function LoadAsistencia(Request $r )
     {
