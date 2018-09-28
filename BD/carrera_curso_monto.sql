112cf60e131bc01a0e2b401d804050bb633b1f1f
shevchenk <jorgeshevchenk@gmail.com>
2018-09-28 18:40:55 -0500

    actualizacion

--- BD/carrera_curso_monto.sql
+++ BD/carrera_curso_monto.sql
@@ -1,1 +1,4 @@
-ALTER TABLE `p_horarios_programados` ADD `carrera` INT NULL AFTER `tolerancia`, ADD `curso` INT NULL AFTER `carrera`, ADD `monto_hora` INT NULL AFTER `curso`;  
+ALTER TABLE `p_horarios_programados` 
+ADD `carrera` INT NULL AFTER `tolerancia`, 
+ADD `curso` INT NULL AFTER `carrera`, 
+ADD `monto_hora` decimal(10,2) NULL AFTER `curso`;  
