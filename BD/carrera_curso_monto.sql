294c9b11001a19ff711b6a57c5035d9bc9ae65a8
shevchenk <jorgeshevchenk@gmail.com>
2019-11-13 11:54:46 -0500

    act

--- BD/carrera_curso_monto.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `p_horarios_programados` 
-ADD `carrera` INT NULL AFTER `tolerancia`, 
-ADD `curso` INT NULL AFTER `carrera`, 
-ADD `monto_hora` decimal(10,2) NULL AFTER `curso`;  
--- BD/menuReporteCOntratos.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `m_privilegios_opciones` (`id`, `privilegio_id`, `opcion_id`, `estado`, `created_at`, `updated_at`, `persona_id_created_at`, 
-`persona_id_updated_at`) VALUES (NULL, '1', '17', '1', '2018-06-03 21:25:03', NULL, '1', NULL)
-
-
-INSERT INTO `m_opciones` (`id`, `menu_id`, `opcion`, `ruta`, `class_icono`, `estado`, `created_at`, `updated_at`, `persona_id_created_at`, 
-`persona_id_updated_at`) VALUES (NULL, '3', 'Contratos', 'reporte.contrato.contrato', 'fa fa-list', '1', '2018-06-03 22:54:04', '0000-00-00 00:00:00', '1', '0')
-
-
